e2379667b997401719777f127a8c58bea1a427bc
Marcio Nitao <marcionitao@gmail.com>
2026-05-31 15:08:19 +0200

    Add Next.js configuration files and update dependencies

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+// @ts-check
+
+/**
+ * @type {import('next').NextConfig}
+ */
+const nextConfig = {
+  /* config options here */
+  allowedDevOrigins: ["192.168.1.7"],
+};
+
+export default nextConfig;
